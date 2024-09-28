f5036a8f80dc17ea5ce9313f83647a97445069a1
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-09-28 16:15:35 +0530

    change

--- backend/userService/controllers/user.controller.js
+++ backend/userService/controllers/user.controller.js
@@ -1,31 +1,43 @@
-const User = require('../models/user.model').User
+const User = require('../models/user.model').User;
 
+const registerUser = async (req, res) => {
+    try {
+        console.log('User Details: ', req.body);
 
-const registerUser = async (req,res) => {
-    try{
-        console.log('user Details: ', req.body)
-        let userData = User({
+      
+        const existingUser = await User.findOne({ username: req.body.username });
+        if (existingUser) {
+            return res.status(400).send('Username already exists');
+        }
+
+        let userData = new User({
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
-        })
-        await userData.save()
-        res.send('User registered!!!')
-    }catch(err){
-        console.log(err)
-        res.send('Something went wrong')
-    }
-}
+        });
 
-const getUser = async(req,res) => {
-    try{
-        User.find({})
-            .then(data => res.send(data))
-            .catch(err => res.send('something went wrong'))
-    }catch(err){
-        console.log(err)
-        res.send('ERR')
+        await userData.save();
+        res.send('User registered!!!');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Something went wrong');
     }
-}
+};
+const getUser = async (req, res) => {
+    try {
+        const usernames = req.query.usernames.split(',');
 
-module.exports = { registerUser,getUser }
+        const users = await User.find({ username: { $in: usernames } });
+
+        if (users.length > 0) {
+            res.send(users);
+        } else {
+            res.status(404).send('No users found');
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Something went wrong');
+    }
+};
+
+module.exports = { registerUser, getUser };
